3754915fbe958c4f242da8825e7d968683d3838d
Issemou ridovic dieudonne <ridovicisseou@gmail.com>
2023-04-02 22:46:47 +0100

    Hopital management systeme end formation

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='HospitalManagementSystem',
+    version='2.7',
+    packages=['package'],
+    url='',
+    license='',
+    author='Issemou',
+    author_email='ridovicisseou@gmail.com',
+    description='Simple Hospital Management System Project'
+)
